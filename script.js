39892759145bf7ab640b80e6b0e1fe117b3fd727
NateWalchenbach <natewalchenbach24@gmail.com>
2021-12-10 08:57:13 -0800

    Added the ability for the user to input a city and fetch the proper json from it

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 //   API KEY: 00fa94081906ea02a56463e3f68c7b6f
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=00fa94081906ea02a56463e3f68c7b6f`)
-  .then(response => response.json())
-  .then(function(data){
-      const searchBtn = document.querySelector('search-button')
-      var city = document.querySelector('user-input').value
-      searchBtn.addEventListener('click', function(){
-          
-      })
-console.log(data);
-  });
+
+//Declared Variables Here
+var userInput = document.getElementById("user-city-input");
+const searchBtnEl = document.getElementById("search-button-el");
+const cityEl = document.getElementById("city-name");
+
+searchBtnEl.addEventListener("click", function () {
+  const trimmedUserInputValue = userInput.value.trim(); //Gets the city that the user types in
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${trimmedUserInputValue}&appid=00fa94081906ea02a56463e3f68c7b6f`;
+  // Fetches the url with the city
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+});
